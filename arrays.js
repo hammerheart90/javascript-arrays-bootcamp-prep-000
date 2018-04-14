76a56e68c74f06c4d51a5faba06b39776742bd25
Colin Brence <cbrence@ramapo.edu>
2018-04-14 15:08:56 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var 
+grilledCheeseIngredients = [
+  'bread'
+  'mild cheese'
+  'sharp cheese'
+  'butter'
+  'tomato'
+  'garlic'
+  ]
+
+var tomatoSauceIngredients = [
+  'tomato'
+  'garlic'
+  'olive oil'
+  'basil'
+  'oregano'
+  ]
+    
+function removeElementFromEndOfArray 
